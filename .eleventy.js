2f5d92cd706ee850c6d8bf3cefc4d09728318380
Jim Bumgardner <jbum@jbum.com>
2023-12-13 15:26:04 -0800

    Added readability functionality.

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ const cagovBuildSystem = require('@cagov/11ty-build-system');
 const markdown = require('./docs/src/11ty/markdown.js');
 const htmlTransform = require('./docs/src/11ty/html-transform.js');
 const forUnsetComponents = require('./docs/src/11ty/for-unset-components.js');
+const fs = require('fs');
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.setLibrary('md', markdown);
@@ -91,6 +92,48 @@ module.exports = function (eleventyConfig) {
     'components/icons/dist/svg': 'cagov-icons-svg',
   });
   eleventyConfig.addPassthroughCopy({ 'components/icons/src/fonts': 'fonts' });
+
+  // generate allfiles?
+  var fileList = [];
+  // for all the .md files in ./docs (recursively)
+  // add the url to the list
+
+  
+  eleventyConfig.addCollection('scorepages', function (collection) {
+    // console.log("--> collection.stuff");
+    // console.trace();
+    var fileList = [];
+    let output = [];
+    collection.getAll().forEach((item) => {
+      var url = item.url;
+      console.log("considering "+url);
+      if (url.match(/^\/((content-design|human-centered-design|product-management)\/)?[\w-]+\/$/)) {
+        var outputPath = url;
+        var inputPath = './docs/pages'+url;
+        // replace trailing / with ".md" on inputPath
+        if (url.match(/\/$/)) {
+          // remove last character of inputPath
+          inputPath = inputPath.slice(0, -1);
+          inputPath = inputPath + '.md';
+        }
+        // if inputPath doesn't exist, log it
+        if (fs.existsSync(inputPath)) {
+          console.log("  will score -> "+inputPath);
+          fileList.push({outputPath:outputPath, inputPath:inputPath});
+          // item.inputPath = inputPath;
+          // item.outputPath = outputPath;
+          // output.push(item);
+        }
+      } else if (url.match(/^\/$/)) {
+        // console.log("  will score -> "+inputPath);
+        fileList.push({outputPath:url, inputPath:'./docs/site/homepage.njk'});
+      }
+    });
+    let abbrevFileList = fileList.filter(item => { return item.outputPath != "_site/blog/index.html";})
+    fs.writeFileSync('./_site_dist/allFiles.json',JSON.stringify(abbrevFileList),'utf8');
+    return output;
+  });
+
 
   return {
     htmlTemplateEngine: 'njk',
