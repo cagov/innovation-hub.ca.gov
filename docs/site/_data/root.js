21af90a608270516a22bd63074baa0ce3111be0e
Jim Bumgardner <jbum@jbum.com>
2023-12-13 14:01:46 -0800

    Performance/readability, in progress.

--- docs/site/_data/root.js
+++ docs/site/_data/root.js
@@ -1,6 +1,6 @@
 const defaultRoot =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:8080'
-    : 'https://designsystem.webstandards.ca.gov';
+    : 'https://hub.innovation.ca.gov';
 
 module.exports = process.env.ROOT || defaultRoot;
